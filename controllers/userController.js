eab3894874467683b6ebe5e7f5d7bb39c163f72d
androidborn <tweaksbuzz@gmail.com>
2019-10-13 09:10:36 +0530

    before sql

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,20 @@
 
 exports.createUser = (req,res) => {
-    // res.status(200)
-    // .json({
-    //     data : req.body,
-    // });
+    res.status(200)
+    .json({
+        data : req.body,
+    });
     console.log(req.body);  
     
-    res.redirect('/');
+    // res.redirect('/');
 };
+
 exports.getUser = (req,res) => {
     // res.status(200)
     // .json({
     //     data : req.body,
     // });
 
-    res.render('index', { pageTitle : 'New Page Title developer'});
+    // res.render('index', { pageTitle : 'New Page Title developer'});
     // console.log(req.body);   
 };
